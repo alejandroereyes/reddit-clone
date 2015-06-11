7c2ec965cca3710d572291c843cd88066f8d4480
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-11 16:16:14 -0500

    updated seed file to seed db with 20 entries

--- readit/db/seeds.rb
+++ readit/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do |variable|
+
+  user =  User.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: Faker::Lorem.word)
+
+  board = Board.create(category: Faker::Lorem.word)
+
+  link = Link.create(post: Faker::Lorem.words, title: Faker::lorem.word, user_id: user.id, board_id: board.id)
+
+  comment = Comment.create(body: Faker::Lorem.words, link_id: link.id, user_id: user.id)
+
+  vote = Vote.create(count: Faker::Number.number(2), user_id: user.id, link_id: link.id)
+
+end

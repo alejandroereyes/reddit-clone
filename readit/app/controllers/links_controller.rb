bee27fd7f65ba168f1891f03107e34fb04cd6b4c
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-11 21:46:59 -0500

    started work on new link page, added sessions controller

--- readit/app/controllers/links_controller.rb
+++ readit/app/controllers/links_controller.rb
@@ -4,9 +4,12 @@ class LinksController < ApplicationController
     @link = Link.find(params[:id])
   end
 
+  def new
+    @link = Link.new
+  end
 
   private
-  def post_params
+  def link_params
     params.require(:link).permit(:title, :body, :user_id, :board_id)
   end
 end

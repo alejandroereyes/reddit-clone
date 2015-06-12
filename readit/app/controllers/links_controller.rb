421c54149d4e6af8bcd79e847620aea2b76c57a9
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-11 19:45:32 -0500

    added link and page for new user, still needs form

--- readit/app/controllers/links_controller.rb
+++ readit/app/controllers/links_controller.rb
@@ -4,6 +4,7 @@ class LinksController < ApplicationController
     @link = Link.find(params[:id])
   end
 
+
   private
   def post_params
     params.require(:link).permit(:title, :body, :user_id, :board_id)
--- readit/config/routes.rb
+++ readit/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :boards
 
+  # get 'links/users/:id', to: 'users/show'
   resources :links
 
   resources :comments

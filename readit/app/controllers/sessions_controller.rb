2d4f309df806a27b55dee66af4ac4f209cceeb80
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-12 14:09:10 -0500

    test log in, redirects to home page as should

--- readit/app/controllers/sessions_controller.rb
+++ readit/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user != nil && user.aunthenticate(params[:password])
+    if user != nil && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: "Welcome #{user.name}!"
     else

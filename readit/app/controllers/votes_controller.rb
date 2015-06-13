fe9605fa9e3f7cfb0ecc96e7f84f22551b87a821
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-13 16:24:34 -0500

    added up and down votes to votes table and corrsponding actions in controller

--- readit/app/controllers/votes_controller.rb
+++ readit/app/controllers/votes_controller.rb
@@ -1,6 +1,38 @@
 class VotesController < ApplicationController
 
   def upvote
-
+    if params[:vote].has_key?(:link_id)
+      if Vote.exists?(params[:vote][:link_id])
+        @vote = Vote.find_by(link_id: params[:vote][:link_id])
+        vote_count = add_vote[:upvote]
+        @vote.upvote = vote_count + 1
+        @vote.user_id = session[:user_id] if session[:user_id]
+        @vote.save
+      else
+        @vote = Vote.new_vote
+        @vote.upvote = 1
+        @vote.link_id = params[:vote][:link_id]
+        @vote.user_id = session[:user_id]
+        @vote.save
+      end # updating link vote cond
+    end # link_id cond
   end
-end
+
+  def downvote
+    if params[:vote].has_key?(:link_id)
+      if Vote.exists?(params[:vote][:link_id])
+        @vote = Vote.find_by(link_id: params[:vote][:link_id])
+        vote_count = add_vote[:downvote]
+        @vote.downvote = vote_count + 1
+        @vote.user_id = session[:user_id] if session[:user_id]
+        @vote.save
+      else
+        @vote = Vote.new_vote
+        @vote.downvote = 1
+        @vote.link_id = params[:vote][:link_id]
+        @vote.user_id = session[:user_id]
+        @vote.save
+      end # updating link vote cond
+    end # link_id cond
+  end
+end #class
--- readit/db/schema.rb
+++ readit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611193731) do
+ActiveRecord::Schema.define(version: 20150613205727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20150611193731) do
     t.integer  "comment_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "upvote"
+    t.integer  "downvote"
   end
 
   add_index "votes", ["comment_id"], name: "index_votes_on_comment_id", using: :btree

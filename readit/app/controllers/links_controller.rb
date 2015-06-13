45bb75ffe32f2306737fb1a59fa1cfc7adca6083
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-13 10:53:40 -0500

    added link title and post to profile page and added log out btn to nav user is persisted

--- readit/app/controllers/links_controller.rb
+++ readit/app/controllers/links_controller.rb
@@ -9,6 +9,20 @@ class LinksController < ApplicationController
     @link = Link.new
   end
 
+  def create
+    authenticate_user!
+    @link = Link.new
+    @link.user_id = session[:user_id]
+    @link.board_id = params[:link][:board_id]
+    @link.title = params[:link][:title]
+    @link.body = params[:link][:body]
+    if @link.save
+      redirect_to @link, notice: "Your link is up!"
+    else
+      render :new
+    end
+  end
+
   private
   def link_params
     params.require(:link).permit(:title, :body, :user_id, :board_id)

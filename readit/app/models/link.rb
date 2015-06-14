4cad7c05bb201ff4787eb643f50b53ebcfdac2fe
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-14 17:41:50 -0500

    updated readme and seed file

--- readit/app/models/link.rb
+++ readit/app/models/link.rb
@@ -3,4 +3,5 @@ class Link < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   belongs_to :board
+  validates :title, :post, :user_id, :board_id, presence: { message: "This section is mandatory" }
 end
